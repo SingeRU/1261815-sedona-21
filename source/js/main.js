8a99a7fe7e1d3ee2118279487b69f651956b001f
ILIDAN <mrster03@gmail.com>
2021-01-25 19:10:16 +0300

    Внёс исправления по указаниям проверяющего наставника

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,11 @@
 var navMain = document.querySelector('.main-nav__container');
 var navToggle = document.querySelector('.main-nav__toggle');
+var navToggleNoJs = document.querySelector('.main-nav__toggle--nojs');
 var navClose = document.querySelector('.main-nav__close');
+var navCloseNoJs = document.querySelector('.main-nav__close--nojs')
+
+navToggleNoJs.classList.remove("main-nav__toggle--nojs");
+navCloseNoJs.classList.remove("main-nav__close--nojs");
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav__container--closed')) {
